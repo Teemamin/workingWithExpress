ddd33de8dc88dbba34a6ba436f6f037b5c372ef1
Teemamin <teemamin@yahoo.com>
2022-08-05 15:53:39 +0400

    added pug template engine

--- routes/shop.js
+++ routes/shop.js
@@ -8,9 +8,8 @@ const adminData = require('./admin');
 
 
 router.get('/',(req, res, next)=>{
-    // console.log('in a middleware one');
-    console.log(adminData.products);
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+   res.render('shop', {prods: products, docTitle: 'shop'});
 })
 
 module.exports = router;
