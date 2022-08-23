afd688c84f33eaec531e6cf7a5f70f6862693571
Teemamin <teemamin@yahoo.com>
2022-08-23 20:33:28 +0400

    pulled changes to delete previously commited env var

--- util/database.js
+++ util/database.js
@@ -1,9 +1,15 @@
-const mysql = require('mysql2');
-const pool = mysql.createPool({
-    host:'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: ''
-});
+const Sequelize = require('sequelize');
 
-module.exports = pool.promise();
+const sequelize = new Sequelize('node-complete','root',)
+
+
+//the config for connectn via mysql2
+// const mysql = require('mysql2');
+// const pool = mysql.createPool({
+//     host:'localhost',
+//     user: 'root',
+//     database: 'node-complete',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
